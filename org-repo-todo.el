100b197869fa5aa01ae552d5fd411475e675695d
Diego Berrocal <cestdiego@gmail.com>
2014-11-25 23:17:30 -0500

    Added TODO feauture

--- org-repo-todo.el
+++ org-repo-todo.el
@@ -22,16 +22,18 @@
 
 ;;; Commentary:
 
-;; This is a simple package for capturing and visiting todo items for the
-;; repository you are currently within. Under the hood it uses `org-capture'
-;; to provide a popup window for inputting `org-mode' checkboxed todo items
-;; (http://orgmode.org/manual/Checkboxes.html) that get saved to a TODO.org
-;; file in the root of the repository.
+;; This is a simple package for capturing and visiting todo items for
+;; the repository you are currently within. Under the hood it uses
+;; `org-capture' to provide a popup window for inputting `org-mode'
+;; checkboxed todo items (http://orgmode.org/manual/Checkboxes.html)
+;; or regular *TODO items that get saved to a TODO.org file in the
+;; root of the repository.
 ;;
 ;; Install is as easy as dropping this file into your load path and setting
 ;; the relevent functions to keybindings of your choice, i.e.:
 ;;
 ;;   (global-set-key (kbd "C-;") 'ort/capture-todo)
+;;   (global-set-key (kbd "C-`") 'ort/capture-checkitem)
 ;;   (global-set-key (kbd "C-'") 'ort/goto-todos)
 ;;
 ;; Using the `C-u' command prefix with either of these commands will
@@ -48,7 +50,15 @@
 (autoload 'vc-svn-root "vc-svn")
 (autoload 'vc-hg-root "vc-hg")
 
-(push '("ort" "Org Repo Todo" checkitem (file (ort/todo-file)))
+(push '("ort/todo" "Org Repo Todo"
+        entry
+        (file+headline (ort/todo-file) "Todos")
+        "* TODO  %?\t\t\t%T\n %i\n Link: %l\n")
+      org-capture-templates)
+
+(push '("ort/checkitem" "Org Repo Checklist Item"
+        checkitem
+        (file+headline (ort/todo-file) "Checklist"))
       org-capture-templates)
 
 (defun ort/todo-file ()
@@ -80,7 +90,19 @@ With the argument DOTEMACS, capture the todo for your .emacs.d's TODO.org file."
   (let ((split-width-threshold nil)
         (split-height-threshold 0)
         (ort/todo-root (ort/find-root dotemacs)))
-    (org-capture nil "ort")
+    (org-capture nil "ort/todo")
+    (fit-window-to-buffer nil nil 5)))
+
+;;;###autoload
+(defun ort/capture-checkitem (&optional dotemacs)
+  "Capture a todo for the current repo in an `org-capture' popup window.
+With the argument DOTEMACS, capture the todo for your .emacs.d's TODO.org file."
+  (interactive "P")
+  ;; make window split horizontally
+  (let ((split-width-threshold nil)
+        (split-height-threshold 0)
+        (ort/todo-root (ort/find-root dotemacs)))
+    (org-capture nil "ort/checkitem")
     (fit-window-to-buffer nil nil 5)))
 
 (provide 'org-repo-todo)
